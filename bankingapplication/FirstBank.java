9e9c59f0d3d678232cba67ec937ce3a8c65ba39a
Osman Shaon <67813420+osmanshaon@users.noreply.github.com>
2021-01-21 16:33:46 -0500

    Add files via upload

---
+++ bankingapplication/FirstBank.java
@@ -0,0 +1,16 @@
+package bankingapplication;
+
+//Visit compscicentral.com, the #1 resource for Computer Science students
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Account os = new Account("Osman Shaon", "A00001");
+
+		Account osman = new Account("Osman", "B00002");
+		osman.showMenu();
+
+	}
+}
